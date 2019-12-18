b09d68042818b044efcd95c19006f8720e632c3e
bala <bala223344@gmail.com>
2019-12-18 07:10:45 +0530

    api update

--- background.js
+++ background.js
@@ -1,34 +1,41 @@
 'use strict';
 
-var api_uri = 'http://safename.io'
+var api_uri = 'http://safename.io/api/search?apikey=yLvSHtXTysS1ug4WyajN'
 
 if(location.protocol == 'https:') {
-     api_uri = 'https://safename.io'
+     api_uri = 'https://safename.io/api/search?apikey=yLvSHtXTysS1ug4WyajN'
 }
 
-//var api_uri = 'http://localhost:8000'
+var api_uri = 'http://localhost:8000/api/search?apikey=yLvSHtXTysS1ug4WyajN'
 var ajaxReq = null
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
    
 
-    if (request.contentScriptQuery == "queryAlias") {
-      var url = api_uri + '/api/alias/' +
-              encodeURIComponent(request.alias);
+    if (request.contentScriptQuery == "queryAnything") {
+      var url = api_uri + '&q=' +
+              request.str;
 
               ajaxReq = $.ajax({
                 url: url, 
                 type: 'get',
-                dataType: 'JSON',
                 beforeSend : function()    {           
                   if(ajaxReq != null) {
                     ajaxReq.abort();
                   }
               },
                 success: function(json) {
+                  
                   sendResponse(json)
                 },
                 error: function(xhr, ajaxOptions, thrownError) {
+                      if (xhr.status == 404) {
+                        sendResponse({error: 404})
+                        return
+
+                      }
+                     
+                      
                         if(thrownError == 'abort' || thrownError == 'undefined') return;
                         alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
                 }
@@ -37,34 +44,7 @@ chrome.runtime.onMessage.addListener(
 
       return true;  // Will respond asynchronously.
     }
-     else if (request.contentScriptQuery == "querySafename") {
-
-
-
-      var url = api_uri + '/api/sn/' +
-              encodeURIComponent(request.sn);
-
-
-              ajaxReq = $.ajax({
-                url: url, 
-                type: 'get',
-                dataType: 'JSON',
-                beforeSend : function()    {           
-                  if(ajaxReq != null) {
-                    ajaxReq.abort();
-                  }
-              },
-                success: function(json) {
-                  sendResponse(json)
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                        if(thrownError == 'abort' || thrownError == 'undefined') return;
-                        alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
-                }
-              });
-  
-      return true;  // Will respond asynchronously.
-    }
+    
   
   });
 
--- program.js
+++ program.js
@@ -1,10 +1,40 @@
 jQuery(function () {
 
+
+      //coins taken from - https://safename.io/coins
+      var coins = 
+  [{"id":1,"text":"ETH"},{"id":2,"text":"BTC"},{"id":75,"text":"XRP"},{"id":12,"text":"BCH"},{"id":53,"text":"LTC"},{"id":35,"text":"EOS"},{"id":11,"text":"BNB"},{"id":15,"text":"BSV"},{"id":60,"text":"XMR"},{"id":9,"text":"AOA"},{"id":10,"text":"BAT"},{"id":14,"text":"BTG"},{"id":16,"text":"BTS"},{"id":17,"text":"BTT"},{"id":19,"text":"BTM"},{"id":21,"text":"LINK"},{"id":22,"text":"CCCX"},{"id":24,"text":"MCO"},{"id":25,"text":"CRO"},{"id":26,"text":"DAI"},{"id":27,"text":"DASH"},{"id":31,"text":"EKT"},{"id":32,"text":"EGT"},{"id":33,"text":"NRG"},{"id":34,"text":"ENJ"},{"id":3,"text":"ZRX"},{"id":38,"text":"GNT"},{"id":42,"text":"HT"},{"id":44,"text":"ICX"},{"id":45,"text":"INB"},{"id":47,"text":"MIOTA"},{"id":50,"text":"KCS"},{"id":51,"text":"LAMB"},{"id":5,"text":"ELF"},{"id":55,"text":"MKR"},{"id":6,"text":"AE"},{"id":67,"text":"ONT"},{"id":70,"text":"QTUM"},{"id":8,"text":"REP"},{"id":82,"text":"XLM"},{"id":84,"text":"XTZ"},{"id":87,"text":"TRX"},{"id":89,"text":"LEO"},{"id":90,"text":"USDC"},{"id":91,"text":"VET"},{"id":98,"text":"ZEC"},{"id":101,"text":"GPL"},{"id":102,"text":"MATIC"}]
+
+
+  
+    var  coin_hyphen_array = []
+
+    for (var i in coins)
+        {
+             var str =   "-"+coins[i].text.toLowerCase();
+             coin_hyphen_array.push(str);
+        }
+
+
+
+       // console.log(coin_hyphen_array);
+      
+        
+
     //NH-chrome-child is to look for click inside the popup container..so it can clear the text selection if any  //text selection will be left intact otherwise
     jQuery('body').append('<div id="NH-chrome-popupover"><div class="NH-popover-content NH-chrome-child "></div></div>')
 
     var popupContent = $(".NH-popover-content")
     var popup = $("#NH-chrome-popupover")
+
+
+    
+
+  
+
+
+
+   // coin_hyphen_array.
 
     jQuery("body").mouseup(function (e) {
         //console.log()
@@ -35,124 +65,113 @@ jQuery(function () {
         //filter out middle spaces..that means it is a word not an address..
         //no special chars allowed like url
         var format = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-
      
+
+        var res = selectionStr.match(/(-\w{3,5}$)/mig);
         
+
+        var found = false
+        
+        //we found a hyphen match
+        if(!found && res && res[0] != -1 && coin_hyphen_array.indexOf(res[0].toLowerCase()) >= 0) {
+            found = true
+        }
+
+        if (!found && selectionStr.startsWith("safename:") || /\w{1,}.safename.io$/.test(selectionStr)) {
+            found = true
+        }
+
+
+
+        if(!found && selectionStr != '' && selectionStr.length > 25 && selectionStr.length <= 42 && selectionStr.indexOf(' ') < 0 && !format.test(selectionStr)){
+            found = true
+        }
+    
+      
         //reverse lookup 
-        if (selectionStr.startsWith("safename:")) {
-            var alias = selectionStr.split(":")[1]
+            if(found) {
+
+            
             popup.show()
             popupContent.html('loading..')
 
             
             chrome.runtime.sendMessage(
                 
-                    {contentScriptQuery: "queryAlias", alias: alias},
+                    {contentScriptQuery: "queryAnything", str: selectionStr},
                     data => {
 
 
-                       // data = JSON.parse(data)
-                    
-                      // console.log(data);
-                     //  return;
+                        
+                        console.log(data);
+                        
                    
-                if(data.length > 0) {
-                    //all recs contains user data..so just take the first one
-                    var user_data = data[0]
-                    var risk_score = ""
+                        if(data.error && data.error == 404) {
+                            var formattedData = ` Records not found. Submit Using  <a class="NH-chrome-child" target="_new" href="https://safeName.io">SafeName.io</a> `;
+                            popup.addClass('not-found')
+                            popupContent.html(formattedData)
+                        }
+                if(data.SN_type) {
                     
-                    var profile = ""
-                    // if (data.address_safename_enabled == 'no') {
-                    //     profile = `Safename: Hidden <br />`
-                    //     risk_score = ""
-                    // }else {
-                    profile = (user_data.address_safename) ? `Safename: <a  class="NH-chrome-child" target="_new" href="http://${user_data.address_safename.toLowerCase()}.safename.io">${user_data.address_safename}</a> <br />` : ""
-                            risk_score = `| Profile Risk Score : ${user_data.profile_risk_score}`
-                   // }
+                
+                    var formattedData = ''
+                   
+                    if(data.SN_type == 'profile'  || data.SN_type == 'default') {
+                        formattedData += (data.public_profile_safename) ? `Safename: <a  class="NH-chrome-child" target="_new" href="http://${data.url}">${data.public_profile_safename}</a> <br /> ${data.public_profile_safename}.safename.io | Profile Risk Score : ${data.profile_risk_score}` : " Profile is hidden "
 
-                   var formattedData = profile
+                        if (data.SN_type == 'default') {
+                               var address = (data.address) ? `<br /> ${data.coin_type} : ${data.address} ` : ""
+                             formattedData += address
 
-                    for (var i=0; i<data.length; i++) {
-                         var addr = data[i]
-                         var status = (addr.address_status == "secure" || addr.address_status == "verified") ? `<span class="green">${addr.address_status}</span>` : `<span class="red">${addr.address_status}</span>`;
-                         formattedData += `${addr.address} <a class="NH-chrome-child" alt="copy address" title="copy address" id="copy-addr"><img  class="NH-chrome-child"   width="20" src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/ionicons/svg/md-copy.svg"></a> <br /> Status :  ${status} ${risk_score}  `;
+                        }
+                     
+
                     }
 
-                        popup.removeClass('not-found')
-                        popupContent.html(formattedData)
-    
-                    
-                }   else {
-                    var formattedData = ` Records not found. Submit Using  <a class="NH-chrome-child" target="_new" href="https://safeName.io">SafeName.io</a> `;
-                    popup.addClass('not-found')
+                    else if(data.SN_type == 'address') {
+
+
+                        var status = (data.address_status == "secure" || data.address_status == "verified") ? `<span class="green">${data.address_status}</span>` : `<span class="red">${data.address_status}</span>`;
+
+                        if(data.address) {
+
+                            formattedData += `${data.coin_type} : ${data.address} <a class="NH-chrome-child" alt="copy address" title="copy address" id="copy-addr"><img  class="NH-chrome-child"   width="20" src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/ionicons/svg/md-copy.svg"></a> <br /> Status :  ${status} | Address risk score : ${data.address_risk_score}  <br />`;
+                            
+                        }
+                       
+
+                
+
+                       
+
+                        formattedData += (data.public_profile_safename) ? `Safename: <a  class="NH-chrome-child" target="_new" href="http://${data.url}">${data.public_profile_safename}</a> <br /> ${data.public_profile_safename}.safename.io | Profile Risk Score : ${data.profile_risk_score}` : " Profile is hidden "
+
+                      
+                     
+
+                    }
+                  
+                    popup.removeClass('not-found')
                     popupContent.html(formattedData)
-                    //popup.hide()
-                }
+
+                    
+                }  
 
                 $("#copy-addr").click(function () {
-                    copy(addr.address)
+                    copy(data.address)
                 })
 
 
                     }
                         );
 
-                 
+                }
 
             
 
-        } else if (selectionStr != '' && selectionStr.length > 25 && selectionStr.length <= 42 && selectionStr.indexOf(' ') < 0 && !format.test(selectionStr)) {
-            
-            popup.show()
         
-            popupContent.html('loading..')
-
-            chrome.runtime.sendMessage({contentScriptQuery: "querySafename", sn: selectionStr},
-                data => {
-                 //   data = JSON.parse(data)
-                
-                if (data && data.address) {
-                    var formattedData = ""
-                    var profile = ""
-                    var risk_score = ""
-
-                    var status = (data.address_status == "secure" || data.address_status == "verified") ? `<span class="green">${data.address_status}</span>` : `<span class="red">${data.address_status}</span>`;
-
-                    if (data.address_safename_enabled == 'no') {
-                        profile = `Safename: Hidden <br />`
-                        risk_score = ""
-                    } else {
-                        profile = (data.address_safename) ? `Safename: <a  class="NH-chrome-child" target="_new" href="http://${data.address_safename.toLowerCase()}.safename.io">${data.address_safename}</a> <br />` : ""
-                        risk_score = `| Profile Risk Score : ${data.profile_risk_score}`
-                    }
-
-
-                    formattedData = ` ${profile}  Status :  ${status} ${risk_score} <br /> ${data.address} <a class="NH-chrome-child" alt="copy address" title="copy address" id="copy-addr"><img  class="NH-chrome-child"   width="20" src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/ionicons/svg/md-copy.svg"></a> `;
-
-                    popup.removeClass('not-found')
-                    popupContent.html(formattedData)
-
-
-                }
-                else {
-                    var formattedData = ` Records not found. Submit Using  <a class="NH-chrome-child" target="_new" href="https://safeName.io">SafeName.io</a> `;
-                    popup.addClass('not-found')
-                    popupContent.html(formattedData)
-                    //popup.hide()
-                }
-
-                $("#copy-addr").click(function () {
-                    copy(selectionStr)
-                })
-
-            });
-
-
-
-
-
-
-        } else {
+        
+      else {
             popup.hide()
         }
 
